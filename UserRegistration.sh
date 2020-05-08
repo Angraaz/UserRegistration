bc3820ca7826776799221ae3bab3981d49754fca
Angraaz <mvdsingh30@gmail.com>
2020-05-08 12:29:27 +0530

    UserDetails

---
+++ UserRegistration.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -x
+
+read -p "Enter first name: " fname
+fNamePattern="^[[:upper:]]{1}[[:lower:]]{3,}"
+
+if [[ $fname =~ $fNamePattern ]]
+then
+	echo "First Name Validated"
+else
+	echo "First Name not Valid"
+fi
+
+read -p "Enter Last name: " lname
+lNamePattern="^[[:upper:]]{1}[[:lower:]]{3,}"
+if [[ $lname =~ $lNamePattern ]]
+then
+	echo "Last name Validated"
+else
+	echo "Last Name Not Valid"
+fi
+
+read -p "Enter Email ID: " email
+emailPattern="^[[:alnum:]]+(([._+-]*)[[:alnum:]]+)*@[[:alnum:]]+.[[:alpha:]]{2,4}([.][[:alpha:]]{2,3})*$"
+
+if [[ $email =~ $emailPattern ]]
+then
+        echo "Email is Validated"
+else
+        echo "Invalid Email"
+fi
+
+read -p "Enter mobile number with country code: " mobno
+mobilePattern="^[1-9]+[0-9]+[\ ]+[1-9]{1}[0-9]{9}$"
+if [[ $mobno =~ $mobilePattern ]]
+then
+	echo "Mobile number validated"
+else
+        echo "Invalid mobile number"
+fi
+
+read -p "Enter password: " passRule4
+passPattern4="^((?=.*([[:digit:]])(?=.*[[:upper:]])(?=.*[[:punct:]]{1})[[:alnum:]]{8,}$"
+if [[ $passRule4 =~ $passPattern4 ]]
+then
+        echo "Password Validated"
+else
+        echo "Invalid Password"
+fi
